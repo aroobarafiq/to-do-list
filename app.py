24ea55867d251aec89800a4051e1b8e610431d49
arooba <aroobarafiq77@gmail.com>
2026-05-19 15:15:20 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Database Model
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+# Home Page - Show Tasks
+@app.route('/')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+# Add Task
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form.get('title')
+    if title:
+        new_task = Task(title=title)
+        db.session.add(new_task)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+# Edit Task
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    task = Task.query.get_or_404(id)
+    if request.method == 'POST':
+        task.title = request.form.get('title')
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', task=task)
+
+# Mark Complete
+@app.route('/complete/<int:id>')
+def complete(id):
+    task = Task.query.get_or_404(id)
+    task.completed = True
+    db.session.commit()
+    return redirect(url_for('index'))
+
+# Delete Task
+@app.route('/delete/<int:id>')
+def delete(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
